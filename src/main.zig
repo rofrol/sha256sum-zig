05c24e43245bdf2c2ee1f182add4b713ed732e35
Roman Frołow <rofrol@gmail.com>
2023-02-15 13:07:09 +0100

    use hashFile from https://github.com/nektro/zig-extras

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,25 @@
 const std = @import("std");
+const string = []const u8;
+
+pub fn hashFile(dir: std.fs.Dir, sub_path: string, comptime Algo: type) ![Algo.digest_length * 2]u8 {
+    const file = try dir.openFile(sub_path, .{});
+    defer file.close();
+    var h = Algo.init(.{});
+    var out: [Algo.digest_length]u8 = undefined;
+    try pipe(file.reader(), h.writer());
+    h.final(&out);
+    var res: [Algo.digest_length * 2]u8 = undefined;
+    var fbs = std.io.fixedBufferStream(&res);
+    try std.fmt.format(fbs.writer(), "{x}", .{std.fmt.fmtSliceHexLower(&out)});
+    return res;
+}
+
+pub fn pipe(reader_from: anytype, writer_to: anytype) !void {
+    var buf: [std.mem.page_size]u8 = undefined;
+    var fifo = std.fifo.LinearFifo(u8, .Slice).init(&buf);
+    defer fifo.deinit();
+    try fifo.pump(reader_from, writer_to);
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -8,20 +29,6 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(args_allocator);
     defer std.process.argsFree(args_allocator, args);
 
-    var file = try std.fs.cwd().openFile(args[1], .{ .mode = .read_only });
-    const file_size = (try file.stat()).size;
-    const allocator = std.heap.page_allocator;
-    var buffer = try allocator.alloc(u8, file_size);
-    try file.reader().readNoEof(buffer);
-
-    const Sha256 = std.crypto.hash.sha2.Sha256;
-    var checksum: [Sha256.digest_length]u8 = undefined;
-    Sha256.hash(buffer, &checksum, .{});
-    var checksum_hash: [Sha256.digest_length * 2]u8 = undefined;
-    _ = try std.fmt.bufPrint(
-        &checksum_hash,
-        "{s}",
-        .{std.fmt.fmtSliceHexLower(&checksum)},
-    );
-    std.debug.print("{s}\n", .{checksum_hash});
+    const hash = try hashFile(std.fs.cwd(), args[1], std.crypto.hash.sha2.Sha256);
+    std.debug.print("{s}\n", .{hash});
 }
